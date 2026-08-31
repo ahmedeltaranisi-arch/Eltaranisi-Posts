01fc687d91446c4ccc70302c3f4debdeb9413588
ahmedeltaranisi-arch <ahmedeltaranisi@example.com>
2026-09-01 00:44:18 +0300

    update logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -32,54 +32,11 @@ export default function Navbar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3 lg:px-8">
           {/* Logo Section */}
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
-            <div
-              className={`bg-[#002B9A] p-6 rounded-2xl inline-flex items-center justify-center ${className}`}
-            >
-              <svg
-                viewBox="0 0 450 150"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-full h-full filter drop-shadow-(--shadow-spec)"
-              >
-                <filter
-                  id="shadow"
-                  x="-10%"
-                  y="-10%"
-                  width="120%"
-                  height="120%"
-                >
-                  <feDropShadow
-                    dx="0"
-                    dy="4"
-                    stdDeviation="3"
-                    floodOpacity="0.3"
-                  />
-                </filter>
-
-                <g filter="url(#shadow)" fill="#FFFFFF">
-                  {/* E */}
-                  <path d="M 45 40 H 68 V 56 H 55 V 68 H 66 V 82 H 55 V 96 H 68 V 112 H 45 Z" />
-                  {/* l */}
-                  <path d="M 72 30 H 82 V 112 H 72 Z" />
-                  {/* t */}
-                  <path d="M 86 52 H 93 V 40 H 101 V 52 H 109 V 66 H 101 V 98 C 101 104 103 105 107 105 H 109 V 112 H 99 C 91 112 86 106 86 96 Z" />
-                  {/* a with wave cut */}
-                  <path d="M 112 68 C 112 55 120 50 131 50 C 142 50 148 56 148 68 V 112 H 138 V 102 C 134 109 127 114 119 114 C 111 114 107 107 107 98 C 107 84 122 81 138 81 V 74 C 138 66 134 62 127 62 C 120 62 116 66 116 72 Z M 138 90 C 127 90 117 92 117 98 C 117 103 121 106 126 106 C 133 106 138 101 138 94 Z" />
-                  {/* r wave effect */}
-                  <path d="M 152 52 H 162 V 64 C 166 56 173 51 182 51 V 64 C 174 62 165 67 163 76 V 112 H 152 Z" />
-                  {/* a */}
-                  <path d="M 186 68 C 186 55 194 50 205 50 C 216 50 222 56 222 68 V 112 H 212 V 102 C 208 109 201 114 193 114 C 185 114 181 107 181 98 C 181 84 196 81 212 81 V 74 C 212 66 208 62 201 62 C 194 62 190 66 190 72 Z M 212 90 C 201 90 191 92 191 98 C 191 103 195 106 200 106 C 207 106 212 101 212 94 Z" />
-                  {/* n with wave */}
-                  <path d="M 226 52 H 236 V 62 C 240 55 248 50 257 50 C 267 50 272 57 272 69 V 112 H 262 V 72 C 262 63 257 59 250 59 C 243 59 236 65 236 74 V 112 H 226 Z" />
-                  {/* i */}
-                  <path d="M 277 52 H 287 V 112 H 277 Z M 277 34 H 287 V 44 H 277 Z" />
-                  {/* s */}
-                  <path d="M 292 98 C 292 107 300 114 312 114 C 323 114 330 107 330 98 C 330 84 300 84 300 71 C 300 64 306 60 312 60 C 318 60 323 63 323 70 H 332 C 332 58 324 51 312 51 C 301 51 291 58 291 69 C 291 83 321 82 321 97 C 321 103 315 106 310 106 C 302 106 298 102 298 98 Z" />
-                  {/* i */}
-                  <path d="M 334 52 H 344 V 112 H 334 Z M 334 34 H 344 V 44 H 334 Z" />
-                </g>
-              </svg>
-            </div>
+            <img
+              className="w-13 h-13 rounded-2xl"
+              src="/my-logo.png"
+              alt="Logo"
+            />
             <span className="self-center text-xl text-[#0b1427] font-bold whitespace-nowrap">
               Eltaranisi Posts
             </span>
